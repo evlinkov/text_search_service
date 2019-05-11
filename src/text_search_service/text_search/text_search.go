9fc57c09e1f62eb1c42421df896cff199410134b
@e.linkov <e.linkov@city-mobil.ru>
2019-05-11 12:27:18 +0300

    little changes

--- src/text_search_service/text_search/text_search.go
+++ src/text_search_service/text_search/text_search.go
@@ -1,6 +1,7 @@
 package text_search
 
 import (
+	"fmt"
 	"github.com/ljfuyuan/suffixtree"
 	"github.com/satori/go.uuid"
 	"sync"
@@ -9,27 +10,31 @@ import (
 type TextSearch struct {
 	tree  Tree
 	words *sync.Map
+	mutex *sync.RWMutex
 }
 
 type Word struct {
-	text       string    `json:"text"`
-	uuid       uuid.UUID `json:"uuid"`
-	popularity int64     `json:"popularity"`
-	index      int       `json:"index"`
+	Text       string    `json:"text"`
+	Uuid       uuid.UUID `json:"uuid"`
+	Popularity int64     `json:"popularity"`
+	Index      int       `json:"index"`
 }
 
 func InitTextSearch(words []Word) *TextSearch {
 	textSearch := &TextSearch{}
 	textSearch.tree = suffixtree.NewGeneralizedSuffixTree()
 	textSearch.words = &sync.Map{}
+	textSearch.mutex = &sync.RWMutex{}
 	for _, word := range words {
-		textSearch.words.Store(word.uuid, word)
-		textSearch.tree.Put(word.text, word.index)
+		textSearch.words.Store(fmt.Sprintf("%v", word.Uuid), word)
+		textSearch.tree.Put(word.Text, word.Index)
 	}
 	return textSearch
 }
 
 func (textSearch *TextSearch) GetAllWords() []Word {
+	defer textSearch.mutex.RUnlock()
+	textSearch.mutex.RLock()
 	words := make([]Word, 0)
 	textSearch.words.Range(func(key interface{}, value interface{}) bool {
 		words = append(words, value.(Word))
