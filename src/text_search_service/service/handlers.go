c0f3126d087c4b64d2d4e0d76a7294985bb224e6
@e.linkov <e.linkov@city-mobil.ru>
2019-05-11 13:12:35 +0300

    v0.1

--- src/text_search_service/service/handlers.go
+++ src/text_search_service/service/handlers.go
@@ -10,12 +10,13 @@ import (
 	"net/http"
 	"os"
 	"path"
+	"sort"
 	"text_search_service/text_search"
 	"text_search_service/util"
 )
 
 const (
-	DELIMITER = "±"
+	MaxLimitSearchResponse = 10
 )
 
 type Connectors struct {
@@ -33,12 +34,26 @@ type WordResponse struct {
 	Popularity int64     `json:"popularity"`
 }
 
+type SearchResponse struct {
+	Words []WordResponse `json:"results"`
+}
+
+type Request struct {
+	Query string `json:"query"`
+}
+
+type SaveResponse struct {
+	Id string `json:"id"`
+}
+
 func (service *Service) InitService(configuration *Configuration) {
 	router := mux.NewRouter()
 	service.configuration = configuration
 	service.initConnectors()
 
 	router.HandleFunc("/get/{uuid}", get(service))
+	router.HandleFunc("/save", save(service))
+	router.HandleFunc("/search", search(service))
 
 	go func() {
 		err := http.ListenAndServe(service.configuration.Address+":"+service.configuration.Port, router)
@@ -46,6 +61,84 @@ func (service *Service) InitService(configuration *Configuration) {
 			log.Fatal("ListenAndServe: ", err)
 		}
 	}()
+}
+
+func save(service *Service) http.HandlerFunc {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		if r.Method != http.MethodPost {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
+		data, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("error while reading body %v", err), http.StatusInternalServerError)
+			log.Printf("error while reading body %v\n", err)
+			return
+		}
+
+		request := Request{}
+		err = json.Unmarshal(data, &request)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		response := SaveResponse{}
+		response.Id = fmt.Sprintf("%v", service.connectors.textSearch.AddWord(request.Query))
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(response)
+	}
+	return http.HandlerFunc(fn)
+}
+
+func search(service *Service) http.HandlerFunc {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		if r.Method != http.MethodGet {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
+		data, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("error while reading body %v", err), http.StatusInternalServerError)
+			log.Printf("error while reading body %v\n", err)
+			return
+		}
+
+		request := Request{}
+		err = json.Unmarshal(data, &request)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		words := service.connectors.textSearch.Search(request.Query)
+		sort.Sort(ByPopularity(words))
+		if len(words) > MaxLimitSearchResponse {
+			words = words[:MaxLimitSearchResponse]
+		}
+		response := SearchResponse{}
+		response.Words = convertAll(words)
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(response)
+	}
+	return http.HandlerFunc(fn)
 }
 
 func get(service *Service) http.HandlerFunc {
@@ -65,7 +158,6 @@ func get(service *Service) http.HandlerFunc {
 		uuid, err := util.ParseStringToUUID(path.Base(r.URL.Path))
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
-			log.Printf("bad request %v\n", err)
 			return
 		}
 
@@ -129,4 +221,20 @@ func convert(word text_search.Word) WordResponse {
 	wordResponse.Uuid = word.Uuid
 	wordResponse.Popularity = word.Popularity
 	return wordResponse
+}
+
+func convertAll(words []text_search.Word) []WordResponse {
+	res := make([]WordResponse, 0, len(words))
+	for _, word := range words {
+		res = append(res, convert(word))
+	}
+	return res
+}
+
+type ByPopularity []text_search.Word
+
+func (a ByPopularity) Len() int      { return len(a) }
+func (a ByPopularity) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
+func (a ByPopularity) Less(i, j int) bool {
+	return a[i].Popularity > a[j].Popularity
 }
--- src/text_search_service/text_search/text_search.go
+++ src/text_search_service/text_search/text_search.go
@@ -4,21 +4,29 @@ import (
 	"fmt"
 	"github.com/ljfuyuan/suffixtree"
 	"github.com/satori/go.uuid"
+	"regexp"
 	"sync"
+	"text_search_service/util"
 )
 
 type TextSearch struct {
 	tree  Tree
-	words *sync.Map
+	words *sync.Map // uuid -> Word
 	mutex *sync.RWMutex
 	index int
+
+	setOfWords      map[string]uuid.UUID
+	mutexSetOfWords *sync.Mutex
+
+	indexToUuid *sync.Map
 }
 
 type Word struct {
-	Text       string    `json:"text"`
-	Uuid       uuid.UUID `json:"uuid"`
-	Popularity int64     `json:"popularity"`
-	Index      int       `json:"index"`
+	Text       string         `json:"text"`
+	Uuid       uuid.UUID      `json:"uuid"`
+	Popularity int64          `json:"popularity"`
+	Index      int            `json:"index"`
+	Re         *regexp.Regexp `json:"-"`
 }
 
 func InitTextSearch(words []Word) *TextSearch {
@@ -26,14 +34,54 @@ func InitTextSearch(words []Word) *TextSearch {
 	textSearch.tree = suffixtree.NewGeneralizedSuffixTree()
 	textSearch.words = &sync.Map{}
 	textSearch.mutex = &sync.RWMutex{}
+	textSearch.setOfWords = make(map[string]uuid.UUID)
+	textSearch.mutexSetOfWords = &sync.Mutex{}
+	textSearch.indexToUuid = &sync.Map{}
+
 	for _, word := range words {
+		word.Re = regexp.MustCompile(word.Text)
 		textSearch.words.Store(fmt.Sprintf("%v", word.Uuid), word)
 		textSearch.tree.Put(word.Text, word.Index)
+		textSearch.indexToUuid.Store(word.Index, word.Uuid)
 		if word.Index > textSearch.index {
 			textSearch.index = word.Index + 1
 		}
+		textSearch.setOfWords[word.Text] = word.Uuid
 	}
 	return textSearch
+}
+
+func (textSearch *TextSearch) AddWord(text string) uuid.UUID {
+	textSearch.mutexSetOfWords.Lock()
+	value, exists := textSearch.setOfWords[text]
+	if exists {
+		textSearch.mutexSetOfWords.Unlock()
+		return value
+	}
+	word := Word{}
+	word.Uuid = util.GenerateUUID()
+	word.Text = text
+	word.Popularity = 1
+	word.Re = regexp.MustCompile(word.Text)
+	word.Index = textSearch.index
+	textSearch.index++
+	textSearch.mutexSetOfWords.Unlock()
+	textSearch.addWord(word)
+	return word.Uuid
+}
+
+func (textSearch *TextSearch) Search(text string) []Word {
+	textSearch.mutex.RLock()
+	indexes := textSearch.tree.Search(text, -1)
+	textSearch.mutex.RUnlock()
+	words := make([]Word, 0)
+	for _, index := range indexes {
+		word, ok := textSearch.indexToUuid.Load(index)
+		if ok {
+			words = append(words, word.(Word))
+		}
+	}
+	return words
 }
 
 func (textSearch *TextSearch) GetWordByUUID(uuid uuid.UUID) (Word, bool) {
@@ -53,4 +101,12 @@ func (textSearch *TextSearch) GetAllWords() []Word {
 		return true
 	})
 	return words
+}
+
+func (textSearch *TextSearch) addWord(word Word) {
+	textSearch.words.Store(fmt.Sprintf("%v", word.Uuid), word)
+	textSearch.indexToUuid.Store(word.Index, word.Uuid)
+	defer textSearch.mutex.Unlock()
+	textSearch.mutex.Lock()
+	textSearch.tree.Put(word.Text, word.Index)
 }
